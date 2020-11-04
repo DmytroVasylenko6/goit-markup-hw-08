6cc79932e57f09a5fe41e334c80b3c4cb7772c41
Dmytro Vasylenko <tredstoun651@uk.net>
2020-11-05 00:14:30 +0200

    добавляет фильтр в портфолио

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,8 +1,14 @@
 import './sass/main.scss'
 import menu from './js/menu'
 import svgxuse from './js/svgxuse'
+import mixitup from 'mixitup'
 
 menu()
 svgxuse()
+
+const portfolioboxEl = document.querySelector('.portfolio-box');
+mixitup(portfolioboxEl);
+ 
+
 
 
